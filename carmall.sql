6ba54fa64165ff863da3ea91a4022bf6482a43d9
linfasfasf <254430304@qq.com>
2016-03-10 21:00:27 +0800

    add sql file

--- carmall.sql
+++ carmall.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2016 年 01 月 27 日 16:19
+-- 生成日期: 2016 年 03 月 09 日 15:30
 -- 服务器版本: 5.5.20
 -- PHP 版本: 5.3.10
 
@@ -23,46 +23,58 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- 表的结构 `group_info`
+--
+
+CREATE TABLE IF NOT EXISTS `group_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `group_id` int(11) NOT NULL,
+  `group_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- 转存表中的数据 `group_info`
+--
+
+INSERT INTO `group_info` (`id`, `group_id`, `group_name`) VALUES
+(1, 627806, '工具');
+
+-- --------------------------------------------------------
+
+--
 -- 表的结构 `pic_info`
 --
 
 CREATE TABLE IF NOT EXISTS `pic_info` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `product_id` int(11) NOT NULL,
-  `pic_name` varchar(255) NOT NULL,
+  `pic_name` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
   `default` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=33 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=17 ;
 
 --
 -- 转存表中的数据 `pic_info`
 --
 
 INSERT INTO `pic_info` (`id`, `product_id`, `pic_name`, `default`) VALUES
-(11, 197816, '56a8de7d0045a.jpg', 1),
-(10, 197816, '56a8de7cf20ad.jpg', 1),
-(9, 197816, '56a8de7cf027b.jpg', 1),
-(12, 400053, '56a8ded9462d7.jpg', 1),
-(13, 400053, '56a8ded947f0d.jpg', 1),
-(14, 400053, '56a8ded94a483.jpg', 1),
-(15, 603990, '56a8df9541fe7.jpg', 1),
-(16, 603990, '56a8df9543eb7.jpg', 0),
-(17, 603990, '56a8df9545f2b.jpg', 0),
-(18, 220389, '56a8dfe7806d2.jpg', 1),
-(19, 220389, '56a8dfe78266a.jpg', 0),
-(20, 220389, '56a8dfe784efd.jpg', 0),
-(21, 669174, '56a8e02fb27b0.jpg', 1),
-(22, 669174, '56a8e02fb4787.jpg', 0),
-(23, 669174, '56a8e02fb72aa.jpg', 0),
-(24, 407055, '56a8e0506bc26.jpg', 1),
-(25, 407055, '56a8e0506e214.jpg', 0),
-(26, 407055, '56a8e0507114d.jpg', 0),
-(27, 101523, '56a8e09f0f439.jpg', 1),
-(28, 101523, '56a8e09f11077.jpg', 0),
-(29, 101523, '56a8e09f13712.jpg', 0),
-(30, 950861, '56a8ea1c32943.jpg', 1),
-(31, 950861, '56a8ea1c34813.jpg', 0),
-(32, 950861, '56a8ea1c37069.jpg', 0);
+(1, 553693, 'thumb56e03f6f2d0c6.JPG', 1),
+(2, 553693, 'thumb56e03f6f2e140.JPG', 0),
+(3, 553693, 'thumb56e03f6f2f21c.JPG', 0),
+(4, 553693, 'thumb56e03f6f30318.JPG', 0),
+(5, 653092, 'thumb56e03fcda90f9.JPG', 1),
+(6, 653092, 'thumb56e03fcdab371.JPG', 0),
+(7, 653092, 'thumb56e03fcdada01.JPG', 0),
+(8, 653092, 'thumb56e03fcdaf69f.JPG', 0),
+(9, 627256, 'thumb56e0401b2442e.JPG', 1),
+(10, 627256, 'thumb56e0401b25765.JPG', 0),
+(11, 627256, 'thumb56e0401b26948.JPG', 0),
+(12, 627256, 'thumb56e0401b27c63.JPG', 0),
+(13, 744660, 'thumb56e0405c155eb.JPG', 1),
+(14, 744660, 'thumb56e0405c1674b.JPG', 0),
+(15, 744660, 'thumb56e0405c17805.JPG', 0),
+(16, 744660, 'thumb56e0405c18b85.JPG', 0);
 
 -- --------------------------------------------------------
 
@@ -73,19 +85,44 @@ INSERT INTO `pic_info` (`id`, `product_id`, `pic_name`, `default`) VALUES
 CREATE TABLE IF NOT EXISTS `product_info` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `product_id` int(11) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `image_url` varchar(255) NOT NULL,
+  `title` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `content` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `group_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=4 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
 --
 -- 转存表中的数据 `product_info`
 --
 
-INSERT INTO `product_info` (`id`, `product_id`, `title`, `image_url`) VALUES
-(1, 10001, '厂家直销车志酷车载吸尘器 手提式大功率干湿两用除尘器汽车礼品', ''),
-(2, 10002, '汽车伸缩式蜡拖 蜡掸 蜡把 除尘掸子 伸缩蜡把 汽车刷 质量稳定', ''),
-(3, 950861, '&lt;span style=&quot;font-size: 13.12px; background-color: rgb(182, 198, 215);&quot;&gt;厂家直销车志酷车载吸尘器 手提式大功率干湿两用除尘器汽车礼品&lt;/span&gt;', '');
+INSERT INTO `product_info` (`id`, `product_id`, `title`, `content`, `group_id`) VALUES
+(1, 553693, '行车记录仪', '商品1介绍', 627806),
+(2, 653092, '车载充气泵', '商品2介绍', 627806),
+(3, 627256, '青花', '商品3介绍', 627806),
+(4, 744660, '扳手', '商品4介绍', 627806);
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `last_login` varchar(255) NOT NULL,
+  `last_ip` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- 转存表中的数据 `user`
+--
+
+INSERT INTO `user` (`id`, `user_name`, `password`, `uid`, `last_login`, `last_ip`) VALUES
+(1, 'rzd', '123321', 123456, '2016-03-09 23:19:39', '127.0.0.1');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
